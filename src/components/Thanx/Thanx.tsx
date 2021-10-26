9d5ddbcef1db9ec5495ceb71e170750bab46cfb9
Operel <oriperelman@gmail.com>
2021-10-26 17:46:37 +0300

    Refactor constants.ts to enums

--- src/components/Thanx/Thanx.tsx
+++ src/components/Thanx/Thanx.tsx
@@ -1,22 +1,19 @@
 import React from 'react';
 import './Thanx.css';
 import { Link } from 'react-router-dom';
-import { ROUTES, TEXTS } from '../../utils/constants';
+import { ROUTES, TNX_TEXTS } from '../../utils/constants';
 
-const Thanx: React.FC = () => {
-  const { thanx } = TEXTS;
-  return (
-    <div className="tnx">
-      <h2>{thanx.tnx}</h2>
-      <p>
-        <span>{thanx.back}</span>
-        {' '}
-        <Link to={ROUTES.HOME}>
-          {thanx.to}
-        </Link>
-      </p>
-    </div>
-  );
-};
+const Thanx: React.FC = () => (
+  <div className="tnx">
+    <h2>{TNX_TEXTS.tnx}</h2>
+    <p>
+      <span>{TNX_TEXTS.back}</span>
+      {' '}
+      <Link to={ROUTES.HOME}>
+        {TNX_TEXTS.to}
+      </Link>
+    </p>
+  </div>
+);
 
 export default Thanx;
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,13 +1,11 @@
-export const ROUTES = Object.freeze({
-  HOME: '/',
-  ANGLES: '/angles',
-  TNX: '/thanx'
-})
+export enum ROUTES {
+  HOME = '/',
+  ANGLES = '/angles',
+  TNX = '/thanx'
+}
 
-export const TEXTS = Object.freeze({
-  thanx: {
-    tnx: 'Thank You',
-    back: 'Back to',
-    to: 'Home Page'
-  }
-})
+export enum TNX_TEXTS {
+  tnx = 'Thank You',
+  back = 'Back to',
+  to = 'Home Page'
+}
